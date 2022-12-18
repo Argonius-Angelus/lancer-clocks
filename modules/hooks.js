0f4062b51227ed8b6c73b1536e80073e8b5fb943
Argonius-Angelus <dragonwingeddestroyer@live.co.uk>
2022-12-18 01:45:05 +0000

    Update hooks.js

--- modules/hooks.js
+++ modules/hooks.js
@@ -5,13 +5,23 @@ import { log } from "./util.js";
 Hooks.once("init", () => {
   log(`Init ${game.data.system.id}`);
   ClockSheet.register();
+  let tradePath = "";
+  if (localStorage.getItem("lancer-clocks.extraPaths") == null) {
+		tradePath = "lancer-clocks"
+		log("No old settings.")
+  }  else {
+	    	log("Old settings found.")
+	    	tradePath = localStorage.getItem("lancer-clocks.extraPaths")
+		localStorage.removeItem("lancer-clocks.extraPaths")
+		log("Old settings deleted.")		
+  }
   game.settings.register("lancer-clocks","extraPaths",{
 		name: 'Extra Lancer Clocks Path',
-		hint: 'This is the directory within the data path for custom clocks. This gets created automatically should it not already exist.',
-		scope: 'client',
+		hint: 'This is the directory within the data path for custom clocks. This gets created automatically should it not already exist. This is stored on a per-world basis for more reliability.',
+		scope: 'world',
 		config: true,
 		type: String,
-		default: 'lancer-clocks',
+		default: tradePath,
 	});
 	let extraPath = game.settings.get("lancer-clocks","extraPaths");
 	if (!(extraPath.endsWith("/"))) {
